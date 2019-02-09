d5bf24aabfc79df1831f0869b40b883ef98d8bf5
Guilherme Faria <guilherme_fgl@hotmail.com>
2019-02-09 09:30:37 -0200

    spring project

---
+++ src/main/java/com/guilhermefgl/crudsellerclient/CrudSellerClientApplication.java
@@ -0,0 +1,14 @@
+package com.guilhermefgl.crudsellerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSellerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSellerClientApplication.class, args);
+	}
+
+}
+
